5e8678ba58cdd2fdcc11afc6076ee027d3d70cc3
Ryota Murakami <fund.blendy@gmail.com>
2026-03-28 21:54:13 +0900

    マップエディタ―をオミット

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,6 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         editor: resolve(__dirname, 'editor.html'),
-        mapEditor: resolve(__dirname, 'map-editor.html'),
       },
     },
   },
